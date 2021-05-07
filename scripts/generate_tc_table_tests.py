3008343f36a57e149fca4a7b7673d204e42617ee
Billy Peake <peake100@gmail.com>
2021-05-06 17:18:20 -0700

    updated with more details and code comments

--- scripts/generate_tc_table_tests.py
+++ scripts/generate_tc_table_tests.py
@@ -12,13 +12,27 @@ from typing import Dict, List, NamedTuple
 
 @dataclasses.dataclass
 class TimebaseInfo:
+    """TimebaseInfo details the framerate/timebase of an event or object."""
+
+    # timebase is is the timebase value of the object, or the rate at which timecode
+    # is being calculated. It may or may not be identical to the framerate.
     timebase: int
+
+    # ntsc is whether this is an ntsc-style timebase.
     ntsc: bool
+
+    # drop_frame is whether this timebase results in drop-frame style timecode.
     drop_frame: bool
+
+    # framerate is the frame rate at which the media is playing back.
     framerate: fractions.Fraction
 
     @classmethod
     def from_element(cls, elm: et.Element) -> "TimebaseInfo":
+        """
+        from element parses a new TimebaseInfo instance from a FCP7XML <timecode/>
+        element.
+        """
         timebase_elm = elm.find("./rate/timebase")
         assert timebase_elm is not None
         assert timebase_elm.text is not None
@@ -49,13 +63,23 @@ class TimebaseInfo:
 
 @dataclasses.dataclass
 class _TimecodeElementInfo:
+    """_TimecodeElementInfo is the parsed data from an FCP7XML <timecode/> element."""
+
+    # base is the timebase data.
     base: TimebaseInfo
 
+    # timecode is the timecode string from the <string/> element.
     timecode: str
+
+    # frame is the frame number from the <frame/> element.
     frame: int
 
     @classmethod
     def from_element(cls, elm: et.Element) -> "_TimecodeElementInfo":
+        """
+        from_element parses a _TimecodeElementInfo instance from an FCP7XML
+        <timecode/> element.
+        """
         timecode_text = elm.findtext("./string")
         assert timecode_text is not None
 
@@ -72,19 +96,49 @@ class _TimecodeElementInfo:
 
 @dataclasses.dataclass
 class TimecodeInfo:
+    """TimecodeInfo holds all the timecode representations for a timecode event."""
+
+    # Timebase rate at which timecode is being calculated. In NTSC cases, it will not
+    # match frame_rate_frac (ex: 24).
     timebase: int
+    # ntsc is whether timecode should be calculated via NTSC representation.
     ntsc: bool
+    # drop_frame is whether this timecode is returned as drop-frame.
     drop_frame: bool
+    # frame_rate_frac is the frame rate as a fractional value
+    # (ex: 24000/1001 for 23.98).
     frame_rate_frac: str
 
+    # timecode is the timecode string parsed from the edl (ex: 01:00:00:00).
     timecode: str
+    # frame is the frame number adjusted by start time parse from the FCP7XML
+    # (ex: 86400).
     frame: int
+    # frame_xml_raw is the raw value from the FCP7XML NOT adjusted by start time.
+    # (ex: 0).
+    frame_xml_raw: int
+    # seconds_rational is a rational representation of the of real-world seconds this
+    # timecode represents (ex: '18018/5').
     seconds_rational: str
+    # seconds_decimal is a decimal representation of the real-world seconds this
+    # timecode represents (ex: '3603.6')
     seconds_decimal: str
+    # ppro_ticks is the Adobe Premiere Pro Ticks value this timecode's real-world time
+    # interval would be represented by (ex: 915372057600000).
+    ppro_ticks: int
+    # ppro_ticks_xml_raw is the raw Adobe Premiere Pro ticks value parsed from the
+    # FCP7XML. It is un-adjusted by media start time (ex: 0).
+    ppro_ticks_xml_raw: int
+    # feet_and_frames is the timecode represented by the number of eet and frames that
+    # would have elapsed on 35mm, 4-perf film (ex: 5400+00).
+    feet_and_frames: str
+    # runtime is the formatted string of the real-world time time elapsed
+    # (ex: 01:00:00.6).
     runtime: str
 
     @classmethod
     def from_element(cls, elm: et.Element) -> "TimecodeInfo":
+        """from_element parses a TimecodeInfo from an FCP7XML <timecode/> element."""
         xml_info = _TimecodeElementInfo.from_element(elm)
         return cls._from_xml_info(xml_info)
 
@@ -93,12 +147,20 @@ class TimecodeInfo:
         cls,
         timecode: str,
         frames: int,
+        frames_raw: int,
+        ppro_ticks_raw: int,
         timebase: TimebaseInfo,
     ) -> "TimecodeInfo":
+        """
+        from_info returns a TimecodeInfo instance based on some parsed / processed info.
+        All missing data will be derived.
+        """
         seconds_rational = frames / timebase.framerate
         seconds_decimal = decimal.Decimal(seconds_rational.numerator) / decimal.Decimal(
             seconds_rational.denominator
         )
+
+        ppro_ticks = round(seconds_rational * 254016000000)
 
         seconds = round(seconds_decimal, 9)
         hours, seconds = divmod(seconds, 60 * 60)
@@ -115,6 +177,9 @@ class TimecodeInfo:
             f"{str(seconds).zfill(2)}{fractal_str}"
         )
 
+        feet, feet_frames = divmod(frames, 16)
+        feet_and_frames = f"{feet}+{str(feet_frames).zfill(2)}"
+
         return cls(
             timebase=timebase.timebase,
             ntsc=timebase.ntsc,
@@ -122,33 +187,66 @@ class TimecodeInfo:
             frame_rate_frac=str(timebase.framerate),
             timecode=timecode,
             frame=frames,
+            frame_xml_raw=frames_raw,
+            ppro_ticks=ppro_ticks,
+            ppro_ticks_xml_raw=ppro_ticks_raw,
             seconds_rational=str(seconds_rational),
             seconds_decimal=str(seconds_decimal),
+            feet_and_frames=feet_and_frames,
             runtime=runtime,
         )
 
     @classmethod
     def _from_xml_info(cls, elm_info: _TimecodeElementInfo) -> "TimecodeInfo":
-        return cls.from_info(elm_info.timecode, elm_info.frame, elm_info.base)
+        """
+        _from_xml_info returns a TimecodeInfo instance from a _TimecodeElementInfo
+        instance.
+        """
+        return cls.from_info(
+            elm_info.timecode,
+            elm_info.frame,
+            frames_raw=elm_info.frame,
+            ppro_ticks_raw=-1,
+            timebase=elm_info.base,
+        )
 
 
 @dataclasses.dataclass
 class EventInfo:
+    """EventInfo holds the data for a timeline event."""
+    # duration_frames is the number of frames this event covers.
     duration_frames: int
+    # source_in holds the timecode info for the source media at the in-point of the
+    # event.
     source_in: TimecodeInfo
+    # source_out holds the timecode info for the source media at the out-point of the
+    # event.
     source_out: TimecodeInfo
+    # record_in holds the timecode info for the sequence/timeline at the in-point of
+    # the event.
     record_in: TimecodeInfo
+    # record_in holds the timecode info for the sequence/timeline at the out-point of
+    # the event.
     record_out: TimecodeInfo
 
 
 @dataclasses.dataclass
 class SequenceInfo:
+    """
+    SequenceInfo contains information about a sequence of timecode events from an NLE
+    timeline.
+    """
+    # start_time holds timecode information about the sequence start time.
     start_time: TimecodeInfo
+    # total_duration_frames holds the total duration of the sequence in frame count.
     total_duration_frames: int
+    # events contains a list of events
     events: List[EventInfo]
 
 
 def parse_sequence_info(root: et.ElementTree) -> SequenceInfo:
+    """parse_sequence_info parses SequenceInfo from an FCP7XML."""
+
     total_duration_frames_text = root.findtext("./sequence/duration")
     assert total_duration_frames_text is not None
     print("TOTAL DURATION:", total_duration_frames_text)
@@ -166,6 +264,8 @@ def parse_sequence_info(root: et.ElementTree) -> SequenceInfo:
     return seq_info
 
 
+# event_regex is the regex or parsing an event from a CMX3600 EDL. It only captures
+# event timecodes, not event numbers or any special properties like markers or respeeds.
 event_regex = re.compile(
     r"(?P<source_in>([0-9]{2}):([0-9]{2}):([0-9]{2}):([0-9]{2}))\s+"
     r"(?P<source_out>([0-9]{2}):([0-9]{2}):([0-9]{2}):([0-9]{2}))\s+"
@@ -175,11 +275,16 @@ event_regex = re.compile(
 
 
 def event_list_from_edl(edl_path: pathlib.Path) -> List[re.Match]:
+    """event_list_from_edl generates a list of event_regex matches from a CMX3600 EDL"""
     with edl_path.open("r") as f:
         return [x for x in event_regex.finditer(f.read())]
 
 
 def write_out(xml_path: pathlib.Path, info: SequenceInfo) -> None:
+    """
+    write_out writes a json of our parsed data out to the same directory and filename
+    of our source XML with '.json' as the extension.
+    """
     source_parent = xml_path.parent
     source_name = xml_path.name.split(".")[0]
 
@@ -196,6 +301,11 @@ def collect_event_info(
     xml_events: List[et.Element],
     start_frame: int,
 ) -> List[EventInfo]:
+    """
+    collect_event_info combines the events from a list of EDL regex matches and FCP7XML
+    <clipitem/> elements in order to have a more complete set of timecode
+    representations generated from an outside program.
+    """
     class FileInfo(NamedTuple):
         base: TimebaseInfo
         start_frame: int
@@ -220,15 +330,25 @@ def collect_event_info(
             file_info = FileInfo(base=base_info, start_frame=file_start_frame)
             event_bases[file_id] = file_info
 
-        source_in_frames = _find_int(xml_event, "in") + file_info.start_frame
-        source_out_frames = _find_int(xml_event, "out") + file_info.start_frame
-        record_in_frames = _find_int(xml_event, "start") + start_frame
-        record_out_frames = _find_int(xml_event, "end") + start_frame
+        source_in_frames_raw = _find_int(xml_event, "in")
+        source_in_frames = source_in_frames_raw + file_info.start_frame
+
+        source_out_frames_raw = _find_int(xml_event, "out")
+        source_out_frames = source_out_frames_raw + file_info.start_frame
+
+        record_in_frames_raw = _find_int(xml_event, "start")
+        record_in_frames = record_in_frames_raw + start_frame
+
+        record_out_frames_raw = _find_int(xml_event, "end")
+        record_out_frames = record_out_frames_raw + start_frame
 
         source_in_tc = edl_event.group("source_in")
         source_out_tc = edl_event.group("source_out")
         record_in_tc = edl_event.group("record_in")
         record_out_tc = edl_event.group("record_out")
+
+        source_in_ppro_ticks_raw = _find_int(xml_event, "pproTicksIn")
+        source_out_ppro_ticks_raw = _find_int(xml_event, "pproTicksOut")
 
         duration = record_out_frames - record_in_frames
         assert duration == source_out_frames - source_in_frames
@@ -238,21 +358,29 @@ def collect_event_info(
             source_in=TimecodeInfo.from_info(
                 source_in_tc,
                 source_in_frames,
+                source_in_frames_raw,
+                source_in_ppro_ticks_raw,
                 file_info.base,
             ),
             source_out=TimecodeInfo.from_info(
                 source_out_tc,
                 source_out_frames,
+                source_out_frames_raw,
+                source_out_ppro_ticks_raw,
                 file_info.base,
             ),
             record_in=TimecodeInfo.from_info(
                 record_in_tc,
                 record_in_frames,
+                record_in_frames_raw,
+                -1,
                 file_info.base,
             ),
             record_out=TimecodeInfo.from_info(
                 record_out_tc,
                 record_out_frames,
+                record_out_frames_raw,
+                -1,
                 file_info.base,
             ),
         )
@@ -263,22 +391,42 @@ def collect_event_info(
 
 
 def _find_int(elm: et.Element, path: str) -> int:
+    """
+    _find_int find an integer value from an xml element at path, or raises if the path
+    does not exist of the text value cannot be converted into an int.
+    """
     int_text = elm.findtext(path)
     assert int_text is not None
     return int(int_text)
 
 
 def main() -> None:
+    """
+    main is our main script. It reads in, combines, and writes our event timecode data
+    from an FCP7XML and CMX3600 EDL of the same sequence.
+    """
+
+    # Get our XML and EDL paths.
     source_xml = pathlib.Path(sys.argv[1])
     source_edl = pathlib.Path(sys.argv[2])
 
+    # Parse the xml into some high-level sequence information.
     xml_tree = et.parse(source_xml)
     info = parse_sequence_info(xml_tree)
 
+    # get a list of regex matches from a our CMX3600 EDL.
     edl_events = event_list_from_edl(source_edl)
+    # get a list of <clipitem/> elements from our FCP7XML.
     xml_events = xml_tree.findall("./sequence/media/video/track/clipitem")
 
-    assert len(edl_events) == len(xml_events)
+    # Assert that we got the same number of elements from each cutlist.
+    edl_event_count = len(edl_events)
+    xml_event_count = len(xml_events)
+    if edl_event_count != xml_event_count:
+        raise RuntimeError(
+            f"event count from EDL ({edl_event_count}) "
+            f"and XML ({xml_event_count}) do not match",
+        )
 
     print("EVENTS FOUND:", len(edl_events))
 
